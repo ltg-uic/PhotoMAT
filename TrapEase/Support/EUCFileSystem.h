5dc645be5d9e771485bfb937594c90dc4f4fc8fb
Aijaz A. Ansari <aijaz@enoor.com>
2014-03-22 07:47:38 -0500

    Added FileSystem

---
+++ TrapEase/Support/EUCFileSystem.h
@@ -0,0 +1,15 @@
+//
+//  EUCFileSystem.h
+//  TrapEase
+//
+//  Created by Aijaz Ansari on 3/22/14.
+//  Copyright (c) 2014 Euclid Software, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EUCFileSystem : NSObject
+
++(uint64_t)getFreeDiskspace;
+
+@end
