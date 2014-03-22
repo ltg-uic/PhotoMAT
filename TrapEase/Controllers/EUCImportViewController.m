d46a21c7606f0b7b5b1c0e3582cc77ae3c4d286d
Aijaz A. Ansari <aijaz@enoor.com>
2014-03-22 17:57:46 -0500

    Added basic collectionview code

--- TrapEase/Controllers/EUCImportViewController.m
+++ TrapEase/Controllers/EUCImportViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "EUCImportViewController.h"
+#import "EUCImportCell.h"
 
 @interface EUCImportViewController ()
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (assign, nonatomic) NSInteger numAlbums;
+@property (assign, nonatomic) NSArray * albums;
 
 @end
 
@@ -31,12 +34,40 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    
+    [self.collectionView registerNib:[UINib nibWithNibName:@"EUCImportCell" bundle:nil] forCellWithReuseIdentifier:@"importCell"];
+    
+    
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+    
 }
+
+#pragma mark - UICollectionViewDataSource
+
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [self.albums count];
+    
+}
+
+
+-(EUCImportCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    EUCImportCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"importCell" forIndexPath:indexPath];
+
+    return cell;
+    
+}
+
+#pragma mark - UICollectionViewDelegate
+
 
 @end
